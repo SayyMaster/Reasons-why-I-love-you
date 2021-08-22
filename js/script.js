fb62b52ebfd084aeb5c717bfe945c24b6d500785
Sayyada Master <66278240+SayyMaster@users.noreply.github.com>
2021-08-23 00:27:27 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -164,7 +164,7 @@ var quotes = [
 // Use it as you please, 'cuz you can't, like, own a color, man.
 // I deleted colors that didn't provide sufficient contrast or seemed particularly grating.
 
-var CSS_COLOR_NAMES = ["Thistle", "SkyBlue", "Silver", "LightSalmon", "Plum", "PaleTurquoise", "NavajoWhite", "LightPink", "LightGrey", "LightBlue"];
+var CSS_COLOR_NAMES = ["Amber", "Teal", "Cyan", "SlateBlue", "Plum", "PaleTurquoise", "Thistle", "LightPink", "Orchid", "LightBlue"];
 
 var quoteIndex = 0;
 var colorIndex = 0;
