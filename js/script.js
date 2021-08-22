6c3fd1a71bb1bbeef6fe1c11b5d25f20a7443766
Sayyada Master <66278240+SayyMaster@users.noreply.github.com>
2021-08-23 00:18:52 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -173,7 +173,7 @@ var intervalID;
 
 // const keyword is not supported in IE
 var NUMBER_OF_QUOTES = 39;
-var CHANGE_INTERVAL = 1000000;
+var CHANGE_INTERVAL = 10000;
 
 // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
